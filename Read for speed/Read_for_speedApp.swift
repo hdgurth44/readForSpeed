ada06300a9fa30878eca85837c30952b7e493c1a
hdgurth44 <hdgurth@gmail.com>
2026-01-19 04:54:23 +0100

    Initial Commit

---
+++ Read for speed/Read_for_speedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Read_for_speedApp.swift
+//  Read for speed
+//
+//  Created by harry angeles on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct Read_for_speedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
